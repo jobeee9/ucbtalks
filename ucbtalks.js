202b90a0ccd3a532cfa8629016afbff73416382b
jobeee9 <151503098+jobeee9@users.noreply.github.com>
2023-12-20 16:37:08 +0800

    Add files via upload

---
+++ ucbtalks.js
@@ -0,0 +1,23 @@
+function homed() {
+    document.body.classList.toggle("hom");
+    document.getElementById('bgimg').style.display = 'block';
+    document.getElementById('bigass').style.display = 'block';
+    }
+    
+    function education() {
+    document.body.classList.toggle("educ");
+    document.getElementById('bgimg').style.display = 'none';
+    document.getElementById('bigass').style.display = 'none';
+    }
+
+function story() {
+    document.body.classList.toggle("stor");
+    document.getElementById('bgimg').style.display = 'none';
+    document.getElementById('bigass').style.display = 'none';
+    }
+
+function about() {
+    document.body.classList.toggle("abou");
+    document.getElementById('bgimg').style.display = 'none';
+    document.getElementById('bigass').style.display = 'none';
+    }
