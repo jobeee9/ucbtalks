895096bdd6699851e90ad349249a1d92525da764
jobeee9 <151503098+jobeee9@users.noreply.github.com>
2023-12-21 09:06:20 +0800

    Add files via upload

---
+++ ucbtalks.js
@@ -0,0 +1,56 @@
+function homed() {
+    document.body.classList.toggle("hom");
+    document.getElementById('bgimg').style.display = 'block';
+    document.getElementById('bigass').style.display = 'block';
+    document.getElementById('posts').style.display = 'flex';
+    document.getElementById('edu').style.display = 'none';
+    document.getElementById('sto').style.display = 'none';
+    document.getElementById('eve').style.display = 'none';
+    document.getElementById('au').style.display = 'none';
+    }
+
+function education() {
+    document.body.classList.toggle("educ");
+    document.getElementById('edu').style.display = 'flex';
+    document.getElementById('bgimg').style.display = 'none';
+    document.getElementById('bigass').style.display = 'none';
+    document.getElementById('posts').style.display= 'none';
+    document.getElementById('sto').style.display = 'none';
+    document.getElementById('au').style.display = 'none';
+    document.getElementById('eve').style.display = 'none';
+     
+    }
+    
+function story() {
+    document.body.classList.toggle("stor");
+    document.getElementById('sto').style.display = 'flex';
+    document.getElementById('bgimg').style.display = 'none';
+    document.getElementById('bigass').style.display = 'none';
+    document.getElementById('posts').style.display= 'none';
+    document.getElementById('edu').style.display = 'none';
+    document.getElementById('au').style.display = 'none';
+    document.getElementById('eve').style.display = 'none';
+    }
+
+function vents() {
+    document.body.classList.toggle("even");
+    document.getElementById('eve').style.display = 'flex';
+    document.getElementById('bgimg').style.display = 'none';
+    document.getElementById('bigass').style.display = 'none';
+    document.getElementById('posts').style.display = 'none';
+    document.getElementById('edu').style.display = 'none';
+    document.getElementById('sto').style.display = 'none';
+    document.getElementById('au').style.display = 'none';
+}
+    
+function about() {
+    document.body.classList.toggle("abou");
+    document.getElementById('au').style.display = 'flex';
+    document.getElementById('bgimg').style.display = 'none';
+    document.getElementById('bigass').style.display = 'none';
+    document.getElementById('posts').style.display= 'none';
+    document.getElementById('edu').style.display = 'none';
+    document.getElementById('sto').style.display = 'none';
+    document.getElementById('eve').style.display = 'none';
+    }
+
